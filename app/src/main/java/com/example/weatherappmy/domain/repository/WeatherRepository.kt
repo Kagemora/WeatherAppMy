c3987fda793a867a8a56c664048ce34ea1d1597a
Vladislaw Ugrovatov <itvalidol@gmail.com>
2024-08-02 16:48:46 +0300

    добавил функции для реализации WeatherRepository

--- app/src/main/java/com/example/weatherappmy/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherappmy/domain/repository/WeatherRepository.kt
@@ -1,4 +1,13 @@
 package com.example.weatherappmy.domain.repository
 
+import com.example.weatherappmy.domain.entities.DailyForecast
+import com.example.weatherappmy.domain.entities.Forecast
+import com.example.weatherappmy.domain.entities.HourlyForecast
+import com.example.weatherappmy.domain.entities.Weather
+
 interface WeatherRepository {
+    suspend fun getWeather(cityId:Int): Weather
+    suspend fun getForecast(cityId: Int): Forecast
+    suspend fun getHourlyForecast(cityId: Int):List<HourlyForecast>
+    suspend fun getDailyForecast(cityId: Int):List<DailyForecast>
 }
