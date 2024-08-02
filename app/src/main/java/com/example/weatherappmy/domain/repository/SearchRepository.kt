c7595b5f4fb0c98e8d4b9dd3e93d8069448309b1
Vladislaw Ugrovatov <itvalidol@gmail.com>
2024-08-02 16:45:36 +0300

    добавил функции для реализации SearchRepository

--- app/src/main/java/com/example/weatherappmy/domain/repository/SearchRepository.kt
+++ app/src/main/java/com/example/weatherappmy/domain/repository/SearchRepository.kt
@@ -1,4 +1,7 @@
 package com.example.weatherappmy.domain.repository
 
+import com.example.weatherappmy.domain.entities.City
+
 interface SearchRepository {
+    suspend fun search(query:String): List<City>
 }
