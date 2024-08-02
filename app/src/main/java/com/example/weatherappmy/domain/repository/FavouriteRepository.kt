c6448db66c7ae18494acb73fd585facae4c010b0
Vladislaw Ugrovatov <itvalidol@gmail.com>
2024-08-02 16:43:11 +0300

    добавил функции для реализации FavouriteRepository

--- app/src/main/java/com/example/weatherappmy/domain/repository/FavouriteRepository.kt
+++ app/src/main/java/com/example/weatherappmy/domain/repository/FavouriteRepository.kt
@@ -1,4 +1,11 @@
 package com.example.weatherappmy.domain.repository
 
+import androidx.lifecycle.LiveData
+import com.example.weatherappmy.domain.entities.City
+
 interface FavouriteRepository {
+    fun favouriteCities(): LiveData<List<City>>
+    fun observeIsFavourite(cityId: Int): LiveData<Boolean>
+    suspend fun addToFavourite(cityId: City)
+    suspend fun removeToFavourite(cityId: Int)
 }
