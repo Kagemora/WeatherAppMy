e246c319438b68e8047a5998e22193e0512c2228
Vladislaw Ugrovatov <itvalidol@gmail.com>
2024-08-26 12:10:30 +0300

    добавил вьюбандинг в финальный фрагмент

--- app/src/main/java/com/example/weatherappmy/presentation/weather/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/weatherappmy/presentation/weather/CurrentWeatherFragment.kt
@@ -7,33 +7,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherappmy.App
 import com.example.weatherappmy.R
+import com.example.weatherappmy.databinding.FragmentCurrentWeatherBinding
+import com.example.weatherappmy.presentation.ViewModelFactory
+import javax.inject.Inject
 
 class CurrentWeatherFragment : Fragment() {
 
+    private var _binding: FragmentCurrentWeatherBinding? = null
+    private val binding get() = _binding!!
 
+    private lateinit var viewModel: CurrentWeatherViewModel
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val component by lazy {
+        (requireActivity().application as App).appComponent
+    }
 
     companion object {
         fun newInstance() = CurrentWeatherFragment()
     }
 
-    private val viewModel: CurrentWeatherViewModel by viewModels()
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        component.inject(this)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_current_weather, container, false)
+        _binding = FragmentCurrentWeatherBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel =
+            ViewModelProvider.create(this, viewModelFactory)[CurrentWeatherViewModel::class.java]
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 }
